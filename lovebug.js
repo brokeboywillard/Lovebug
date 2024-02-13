5964dbc3f844acae5295695ae0d8529e40aa5b0f
brokeboywillard <103409074+brokeboywillard@users.noreply.github.com>
2024-02-13 15:29:15 -0500

    Add files via upload

---
+++ lovebug.js
@@ -0,0 +1,17 @@
+// Get the button elements
+var yesButton = document.getElementById('valentine-button');
+var noButton = document.getElementById('no-button');
+
+// Add event listener to the "No" button
+noButton.addEventListener('click', function() {
+    // Enlarge the "Yes" button text
+    var currentFontSize = window.getComputedStyle(yesButton, null).getPropertyValue('font-size');
+    var newSize = parseInt(currentFontSize) + 4 + 'px';
+    yesButton.style.fontSize = newSize;
+});
+
+// Add event listener to the "Yes" button
+yesButton.addEventListener('click', function() {
+    // Display a sweet message
+    alert('Yes, I will be your Valentine! 💖');
+});
